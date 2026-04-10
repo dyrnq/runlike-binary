4afcdcf2bb56f8b4f1b73345ad838fd4e682ea8f
dyrnq <dyrnq@qq.com>
2026-04-10 15:28:38 +0800

    optimization

--- go.sh
+++ go.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 
+echo "branch version=${1}"
 pip install docker click pyinstaller
 git clone -b $1 --depth 1 https://github.com/lavie/runlike.git
 
 pushd runlike >/dev/null || exit 1
 
-cat << 'EOF' > runlike_single.py
+cat << 'EOF' > runlike.py
 #!/usr/bin/env python
 import sys
 import os
 import json
 import click
 import docker
-
+from json import loads
+from subprocess import check_output, STDOUT, CalledProcessError
+from shlex import quote
+import re
 # --- inject runlike/inspector.py ---
 EOF
 
@@ -21,21 +25,21 @@ EOF
 (
 grep -v "import" runlike/inspector.py
 echo -e "\n# --- injest runlike/runlike.py ---"
-sed -n '/@click.command/,$p' runlike/runlike.py | grep -v "from .inspector" 
-) >> runlike_single.py
+sed -n '/@click.command/,$p' runlike/runlike.py | grep -v "from .inspector" | grep -v "from inspector"
+) >> runlike.py
 
-echo "--- done runlike_single.py ---"
+echo "--- done runlike.py ---"
 
-cat runlike_single.py
+cat runlike.py
 pyinstaller \
 --onefile \
 --clean \
---name runlike_final \
+--name runlike \
 --collect-all docker \
 --collect-all click \
-runlike_single.py
+runlike.py
 ls -lh ./dist
-./dist/runlike_final --help
+./dist/runlike --help
 
 
 
